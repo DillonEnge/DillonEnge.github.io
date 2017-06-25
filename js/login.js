8023acbc59226ae15c2126eae3b8ebe421902058
Dillon Enge <dillonenge@Dillons-MBP.hsd1.ga.comcast.net>
2017-06-25 03:53:19 -0400

    Improved login.

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ function login(){
     if(document.getElementById('input').value === 'dillon'){
         window.location.href = "about.html";
     } else{
-        document.getElementById('input').value = 'Incorrect, try again.';
+        document.getElementById('input').placeholder = 'Incorrect Password';
+        document.getElementById('input').value = '';
     }
 }
