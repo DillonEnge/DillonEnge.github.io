426fae22c831ed7c22c183e39f8b7960a1f6cdb0
Dillon Enge <dillon.enge@gmail.com>
2017-07-17 22:17:07 -0400

    Added temp color scheme.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 function login(){
     if(document.getElementById('input').value === 'dillon'){
-        window.location.href = "intro.html";
+        window.location.href = "about.html";
     } else{
         document.getElementById('input').placeholder = 'Incorrect Password';
         document.getElementById('input').value = '';
